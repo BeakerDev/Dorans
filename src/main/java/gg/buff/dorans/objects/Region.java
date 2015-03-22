399cb17d5df707f1e1f046c132aea468b146b010
Stephen Williams <me@alta189.com>
2015-03-22 15:28:51 -0400

    Region Endpoints

--- src/main/java/gg/buff/dorans/objects/Region.java
+++ src/main/java/gg/buff/dorans/objects/Region.java
@@ -1,29 +1,47 @@
 package gg.buff.dorans.objects;
 
 public enum Region {
-    NA("NA1"),
-    EUW("EUW1"),
-    EUNE("EUN1"),
-    KR("KR"),
-    BR("BR1"),
-    LAS("LA2"),
-    LAN("LA1"),
-    OCE("OC1"),
-    TR("TR1"),
-    RU("RU"),
-    GLOBAL("");
+    NA("NA1", "https://na.api.pvp.net/"),
+    EUW("EUW1", "https://euw.api.pvp.net/"),
+    EUNE("EUN1", "https://eune.api.pvp.net/"),
+    KR("KR", "https://kr.api.pvp.net/"),
+    BR("BR1", "htpp://br.api.pvp.net/"),
+    LAS("LA2", "htpp://las.api.pvp.net/"),
+    LAN("LA1", "htpp://lan.api.pvp.net/"),
+    OCE("OC1", "htpp://oce.api.pvp.net/"),
+    TR("TR1", "htpp://tr.api.pvp.net/"),
+    RU("RU", "htpp://ru.api.pvp.net/"),
+    GLOBAL("", "htpp://global.api.pvp.net/");
 
     private final String platformId;
+	private final String endpoint;
+	private final String observerUrl;
+	private final String resourceUrl;
 
-    private Region(String platformId) {
+    private Region(String platformId, String endpoint) {
         this.platformId = platformId;
+	    this.endpoint = endpoint;
+	    this.observerUrl = endpoint + "observer-mode/rest/";
+	    this.resourceUrl = endpoint + "api/lol/";
     }
 
     public String getPlatformId() {
         return platformId;
     }
 
-    @Override
+	public String getEndpoint() {
+		return endpoint;
+	}
+
+	public String getObserverUrl() {
+		return observerUrl;
+	}
+
+	public String getResourceUrl() {
+		return resourceUrl;
+	}
+
+	@Override
     public String toString() {
         return name().toLowerCase();
     }
